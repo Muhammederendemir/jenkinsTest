d1251f22516c3d27acee2f7127bdc619b78c864b
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-07-22 10:56:57 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,7 +8,7 @@ pipeline {
             if (env.BRANCH_NAME == 'master') {
                 slackSend baseUrl: 'https://hooks.slack.com/services/', channel: 'jenkins', color: 'good', iconEmoji: '', message: 'Brach master', teamDomain: 'javadevelopercorp58', tokenCredentialId: 'slack_Token_webhook', username: ''
             } else {
-                echo 'I execute elsewhere'
+                echo 'I'
             }
         }
     }
